2be134e456c192abf57083fec58209e81b3b3430
Yamuna <Yamuna@DESKTOP-2INU3DQ>
2026-09-08 06:29:01 +0530

    code update

--- Se_Java_Github/src/test/java/java_Code/StringReverse.java
+++ Se_Java_Github/src/test/java/java_Code/StringReverse.java
@@ -3,7 +3,19 @@ package java_Code;
 public class StringReverse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String ip="Madam";
+		ip=ip.toLowerCase();
+		String rev="";
+		StringBuffer sb=new StringBuffer(ip);
+		
+		System.out.println(sb.reverse());
+		
+		if(ip.equals(rev)) {
+			System.out.println("Given ip string is a Palindrome");
+		}
+		else {
+			System.out.println("Not a palindrome");
+		}
 
 	}
 
